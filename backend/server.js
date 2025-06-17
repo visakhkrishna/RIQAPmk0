a83ac10940c9c6e3611dbdc7e83bc55318dc3f1b
Visakh V Krishna <vkrishna.visakh@gmail.com>
2025-06-17 11:13:57 +0200

    edit
    edit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-javascriptconst express = require('express');
+const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const ExcelJS = require('exceljs');
